42b22f3299d61f3dd2d024d5cfa770c69c835ddc
PanGan21 <ganelis.pan@gmail.com>
2023-10-05 17:22:12 +0200

    make tests compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
+#[derive(Default, Clone, Copy)]
+pub struct KvStore {}
 
+impl KvStore {
+    pub fn new() -> Self {
+        panic!()
+    }
+
+    pub fn set(self, _key: String, _value: String) {
+        panic!()
+    }
+
+    pub fn get(self, _key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn remove(self, _key: String) {
+        panic!()
+    }
+}
