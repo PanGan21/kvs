35fe0991a6ba2f29d4b11cf06d1e089519c60ad9
PanGan21 <ganelis.pan@gmail.com>
2023-10-16 21:02:58 +0200

    add kvs engine trait

--- src/engines/mod.rs
+++ src/engines/mod.rs
@@ -1,1 +1,15 @@
-pub struct KvsEngine {}
+use crate::Result;
+
+pub trait KvsEngine {
+    /// Set the value of a string key to a string.
+    /// Return an error if the value is not written successfully.
+    fn set(&mut self, key: String, value: String) -> Result<()>;
+
+    /// Get the string value of a string key. If the key does not exist, return None.
+    /// Return an error if the value is not read successfully.
+    fn get(&mut self, key: String) -> Result<Option<String>>;
+
+    /// Remove a given string key.
+    /// Return an error if the key does not exit or value is not read successfully.
+    fn remove(&mut self, key: String) -> Result<()>;
+}
