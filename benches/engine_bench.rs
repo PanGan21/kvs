4f04de63072057b544fd2ad6d60c37203fd714b1
PanGan21 <ganelis.pan@gmail.com>
2023-11-26 11:27:04 +0200

    make benchmarks async

--- benches/engine_bench.rs
+++ benches/engine_bench.rs
@@ -1,4 +1,6 @@
+use criterion::async_executor::FuturesExecutor;
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use kvs::thread_pool::RayonThreadPool;
 use kvs::{KvStore, KvsEngine, SledKvsEngine};
 use rand::prelude::*;
 use tempfile::TempDir;
@@ -6,14 +8,21 @@ use tempfile::TempDir;
 fn set_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("set_bench");
     group.bench_function("kvs", |b| {
-        b.iter_batched(
+        b.to_async(FuturesExecutor).iter_batched(
             || {
                 let temp_dir = TempDir::new().unwrap();
-                (KvStore::open(temp_dir.path()).unwrap(), temp_dir)
+                (
+                    KvStore::<RayonThreadPool>::open(temp_dir.path(), 8).unwrap(),
+                    temp_dir,
+                )
             },
-            |(mut store, _temp_dir)| {
+            |(store, _temp_dir)| async move {
                 for i in 1..(1 << 12) {
-                    store.set(format!("key{}", i), "value".to_string()).unwrap();
+                    store
+                        .clone()
+                        .set(format!("key{}", i), "value".to_string())
+                        .await
+                        .unwrap();
                 }
             },
             BatchSize::SmallInput,
@@ -21,14 +30,21 @@ fn set_bench(c: &mut Criterion) {
     });
 
     group.bench_function("sled", |b| {
-        b.iter_batched(
+        b.to_async(FuturesExecutor).iter_batched(
             || {
                 let temp_dir = TempDir::new().unwrap();
-                (SledKvsEngine::new(sled::open(&temp_dir).unwrap()), temp_dir)
+                (
+                    SledKvsEngine::<RayonThreadPool>::new(sled::open(&temp_dir).unwrap(), 8)
+                        .unwrap(),
+                    temp_dir,
+                )
             },
-            |(mut db, _temp_dir)| {
+            |(db, _temp_dir)| async move {
                 for i in 1..(1 << 12) {
-                    db.set(format!("key{}", i), "value".to_string()).unwrap();
+                    db.clone()
+                        .set(format!("key{}", i), "value".to_string())
+                        .await
+                        .unwrap();
                 }
             },
             BatchSize::SmallInput,
@@ -42,16 +58,20 @@ fn get_bench(c: &mut Criterion) {
     for i in &vec![8, 12, 16, 20] {
         group.bench_with_input(format!("kvs_{}", i), i, |b, i| {
             let temp_dir = TempDir::new().unwrap();
-            let mut store = KvStore::open(temp_dir.path()).unwrap();
+            let store = KvStore::<RayonThreadPool>::open(temp_dir.path(), 8).unwrap();
             for key_i in 1..(1 << i) {
-                store
-                    .set(format!("key{}", key_i), "value".to_string())
-                    .unwrap();
+                tokio::spawn(
+                    store
+                        .clone()
+                        .set(format!("key{}", key_i), "value".to_string()),
+                );
             }
-            let mut rng = SmallRng::from_seed([0; 32]);
-            b.iter(|| {
+            let rng = SmallRng::from_seed([0; 32]);
+            b.to_async(FuturesExecutor).iter(|| async {
                 store
-                    .get(format!("key{}", rng.gen_range(1..i.to_owned())))
+                    .clone()
+                    .get(format!("key{}", rng.clone().gen_range(1..i.to_owned())))
+                    .await
                     .unwrap();
             })
         });
@@ -59,14 +79,16 @@ fn get_bench(c: &mut Criterion) {
     for i in &vec![8, 12, 16, 20] {
         group.bench_with_input(format!("sled_{}", i), i, |b, i| {
             let temp_dir = TempDir::new().unwrap();
-            let mut db = SledKvsEngine::new(sled::open(&temp_dir).unwrap());
+            let db =
+                SledKvsEngine::<RayonThreadPool>::new(sled::open(&temp_dir).unwrap(), 8).unwrap();
             for key_i in 1..(1 << i) {
-                db.set(format!("key{}", key_i), "value".to_string())
-                    .unwrap();
+                tokio::spawn(db.clone().set(format!("key{}", key_i), "value".to_string()));
             }
-            let mut rng = SmallRng::from_seed([0; 32]);
-            b.iter(|| {
-                db.get(format!("key{}", rng.gen_range(1..i.to_owned())))
+            let rng = SmallRng::from_seed([0; 32]);
+            b.to_async(FuturesExecutor).iter(|| async {
+                db.clone()
+                    .get(format!("key{}", rng.clone().gen_range(1..i.to_owned())))
+                    .await
                     .unwrap();
             })
         });
